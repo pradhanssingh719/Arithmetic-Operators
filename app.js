c1e5c41fd2128ab0468b3d6bbb7c25f79cea8ec5
pradhanssingh719 <pradhansingh719@gmail.com>
2020-02-03 15:28:13 +0530

    multiplication table

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ function myFunction() {
  document.getElementById("one").innerHTML = "Addition";
 }
 function multiplication(num){
-  return num * num;
+  for(let i=1; i<=10; i++){
+    let a = num*i;
+    document.getElementById(i).innerHTML = a;
+  }
+}
+function multiplicationTable(){
+  var x = document.getElementById("num").value;
+  multiplication(x);
 }
