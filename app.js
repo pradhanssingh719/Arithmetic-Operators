c1a3f55dcdb915114c5e1c9f48dc213ca261044a
pradhanssingh719 <pradhansingh719@gmail.com>
2020-02-06 17:24:17 +0530

    clock added

--- app.js
+++ app.js
@@ -15,3 +15,33 @@ function multiplicationTable(){
     multiplication(x);
   }
 }
+function currentTime(){
+  let today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  h = timeConverter(h);
+  document.getElementById("time").innerHTML = h + ':' + m + ':' + s;
+  let t = setTimeout(currentTime, 1000);
+}
+function timeConverter(hour){
+  if(hour < 12){
+    document.getElementById('ampm').innerHTML = " am";
+  }
+  if(hour > 12){
+    hour -= 12;
+    document.getElementById('ampm').innerHTML = " pm";
+  }
+  if(hour < 10){
+    hour = '0'+hour;
+  }
+  return hour;
+}
+function checkTime(val){
+    if(val < 10){
+      val = '0'+val;
+    }
+    return val;
+}
